e564a6c0e676350f89ef7e98c87136d4a0cb83f6
Oguz Dastan <oguzx@live.dk>
2019-03-06 22:23:31 +0100

    Add project files.

---
+++ KonsolApp_DB_/DBClient.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace KonsolApp_DB_
+{
+    class DBClient
+    {
+        public void StartConnection(string queryString)
+        {
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=HotelDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Connection.Open();
+                command.ExecuteNonQuery();
+
+                /*SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    int id = reader.GetInt32(0);
+                    string name = reader.GetString(1);
+
+                    Console.WriteLine($"{id}: {name}");
+                }*/
+
+            }
+        }
+
+        
+    }
+}
---
+++ KonsolApp_DB_/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonsolApp_DB_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string queryInsert = "INSERT INTO DemoHotel (Hotel_No, Name, Address) VALUES (10, 'TestData', 'Testvej')";
+            string queryDelete = "DELETE FROM DemoHotel WHERE Hotel_No = 10";
+            string queryUpdate = "UPDATE DemoHotel SET Name = 'Det nye navn' WHERE Hotel_No = 1";
+            DBClient data_1 = new DBClient();
+            data_1.StartConnection(queryUpdate);
+            
+
+        }
+    }
+}
